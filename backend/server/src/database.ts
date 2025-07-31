865fd4998a5131bf03fb58f246bbc54f625ee547
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2025-07-31 15:44:18 -0500

    Add endpoint to query YT and write to Firestore

--- backend/server/src/database.ts
+++ backend/server/src/database.ts
@@ -1,5 +1,6 @@
 import { initializeApp, applicationDefault } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
+import z from "zod";
 
 export class Database {
   private firestore: Firestore;
@@ -8,6 +9,16 @@ export class Database {
     initFirebaseAdmin();
     this.firestore = getFirestore();
   }
+
+  async putPosts(posts: Post[]) {
+    const batch = this.firestore.batch();
+    for (const post of posts) {
+      batch.set(this.firestore.collection("posts").doc(post.id), post, {
+        merge: true,
+      });
+    }
+    await batch.commit();
+  }
 }
 
 function initFirebaseAdmin() {
@@ -15,3 +26,26 @@ function initFirebaseAdmin() {
     credential: applicationDefault(),
   });
 }
+
+const youtubeVideoSchema = z.object({
+  videoId: z.string(),
+  title: z.string(),
+  url: z.url(),
+  publishedAt: z.string(),
+  updatedAt: z.string(),
+  thumbnailUrl: z.url(),
+  description: z.string(),
+  likes: z.int(),
+  views: z.int(),
+});
+
+export type YoutubeVideo = z.infer<typeof youtubeVideoSchema>;
+
+const postSchema = z.object({
+  type: "youtube",
+  id: z.string(),
+  publishedAt: z.string(),
+  data: z.union([youtubeVideoSchema]),
+});
+
+export type Post = z.infer<typeof postSchema>;
--- backend/server/src/router.ts
+++ backend/server/src/router.ts
@@ -1,11 +1,66 @@
 import { Request, Response, Router } from "express";
-import { Database } from "./database.js";
+import { Database, Post, YoutubeVideo as YoutubeVideo } from "./database.js";
+import axios from "axios";
+import xml2js from "xml2js";
 
 export function router(database: Database): Router {
   const router = Router();
 
-  router.get("/pwnage", async (req: Request, res: Response) => {
-    return res.json({ test: 1234 });
+  router.post("/youtube", async (req: Request, res: Response) => {
+    const channelUrl =
+      "https://www.youtube.com/feeds/videos.xml?channel_id=UCKkt1XBxvF6EKalH8G2vP2A";
+
+    let response: any;
+    try {
+      response = await axios.get(channelUrl, { responseType: "text" });
+    } catch (error) {
+      return res.status(500).json({ error: "Error fetching the YouTube feed" });
+    }
+
+    const parser = new xml2js.Parser();
+    let result: any;
+    try {
+      result = await parser.parseStringPromise(response.data);
+    } catch (e) {
+      return res.status(500).json({ error: "Error parsing XML" });
+    }
+
+    const entries = result.feed.entry || [];
+    const videos: YoutubeVideo[] = entries.map((entry: any): YoutubeVideo => {
+      return {
+        videoId: entry["yt:videoId"][0],
+        title: entry.title[0],
+        url: entry.link[0].$.href,
+        publishedAt: entry.published[0],
+        updatedAt: entry.updated[0],
+        thumbnailUrl: entry["media:group"][0]["media:thumbnail"][0].$.url,
+        description: entry["media:group"][0]["media:description"][0],
+        likes:
+          entry["media:group"][0]["media:community"][0]["media:starRating"][0].$
+            .count,
+        views:
+          entry["media:group"][0]["media:community"][0]["media:statistics"][0].$
+            .views,
+      };
+    });
+
+    const posts: Post[] = videos.map((video) => {
+      return {
+        type: "youtube_video",
+        id: `youtube_${video.videoId}`,
+        publishedAt: video.publishedAt,
+        data: video,
+      };
+    });
+
+    try {
+      await database.putPosts(posts);
+    } catch (e) {
+      console.error(e);
+      return res.status(500).json({ error: "Error saving posts" });
+    }
+
+    return res.status(200);
   });
 
   return router;
