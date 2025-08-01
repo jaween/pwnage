c65ae893955b6d84f310aa7becf7e0f4d27cfb9f
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2025-07-31 22:01:29 -0500

    Add routing

--- app/lib/pwnage_app.dart
+++ app/lib/pwnage_app.dart
@@ -1,15 +1,74 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:pwnage/feed_page.dart';
+import 'package:pwnage/not_found_page.dart';
+import 'package:pwnage/transition.dart';
 
 class PwnageApp extends StatelessWidget {
   const PwnageApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(child: Text('Pwnage')),
-      ),
+    return _RouterBuilder(
+      navigatorObservers: [],
+      builder: (context, router) {
+        return MaterialApp.router(routerConfig: router);
+      },
+    );
+  }
+}
+
+class _RouterBuilder extends StatefulWidget {
+  final List<NavigatorObserver> navigatorObservers;
+  final Widget Function(BuildContext context, GoRouter router) builder;
+
+  const _RouterBuilder({
+    super.key,
+    required this.navigatorObservers,
+    required this.builder,
+  });
+
+  @override
+  State<_RouterBuilder> createState() => _RouterBuilderState();
+}
+
+class _RouterBuilderState extends State<_RouterBuilder> {
+  late final GoRouter _router;
+
+  @override
+  void initState() {
+    super.initState();
+    _router = _initRouter();
+  }
+
+  @override
+  void dispose() {
+    _router.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(context, _router);
+  }
+
+  GoRouter _initRouter({String? initialLocation}) {
+    return GoRouter(
+      debugLogDiagnostics: kDebugMode,
+      observers: widget.navigatorObservers,
+      initialLocation: initialLocation ?? '/',
+      overridePlatformDefaultLocation: true,
+      errorBuilder: (context, state) => NotFoundPage(),
+      routes: [
+        GoRoute(
+          path: '/',
+          name: 'feed',
+          pageBuilder: (context, state) {
+            return TopLevelTransitionPage(child: FeedPage());
+          },
+        ),
+      ],
     );
   }
 }
