4e1eba47630c9a2ab2fcd74ecbbaae10f0c592d1
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2025-08-03 20:19:14 -0500

    Lock app to portrait orientation

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pwnage/pwnage_app.dart';
 import 'package:pwnage/services/api_service.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   const apiBaseUrl = String.fromEnvironment('SERVER_BASE_URL');
   final api = ApiService(baseUrl: apiBaseUrl);
 
