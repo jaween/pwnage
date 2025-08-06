269e49da7444ac6943156796a9693e76f7587a21
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2025-08-05 19:34:59 -0500

    Remove hash from URL

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pwnage/pwnage_app.dart';
 import 'package:pwnage/services/api_service.dart';
@@ -8,6 +9,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   BrowserContextMenu.disableContextMenu();
+  usePathUrlStrategy();
 
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
